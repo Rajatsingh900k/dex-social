65a1272eea59a1c1b321687ecba4913cbdf88c0f
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-11 21:57:29 +0530

    Added utils: template setup for ApiErrors, ApiResponse, asynHandler.

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//wrapper function for async-await
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+export {asyncHandler}
+// or
+
+// const asyncHandler=(func)=>{
+//    async (req,res,next)=>{
+//     try{
+//         await func(req,res,next)
+//     }catch(e){
+//         e.status(e.code || 500).json({
+//             success: false,
+//             message: e.message
+//         })
+//     }
+//    }
+// }
