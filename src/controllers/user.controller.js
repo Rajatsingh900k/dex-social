611569fcf59488b0f3f4fa300d586e83ca50ed77
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-16 22:03:43 +0530

    fix: ensure user existence check is awaited and clean up unused code in registerUser function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,6 @@ const registerUser = asyncHandler(async (req,res)=>{
     
     //form or json data will be handled with req.body
     const {fullname, email, username, password}=req.body
-
     // if(fullname===""){
     //     throw new ApiError(400, "fullname is required.")
     // }else{
@@ -30,17 +29,23 @@ const registerUser = asyncHandler(async (req,res)=>{
         throw new ApiError(400, "All fields are required.")
     }
 
-    const existedUser=User.findOne({
-            $or:[{email},{username}]
+    const existedUser=await User.findOne({
+            $or:[{email},{username}]// email || username
     })
     
     if(existedUser){
         throw new ApiError(409, "User with email or username already exists.")
     }
-
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
     
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    
+    // checking in classsical way
+    let coverImageLocalPath
+    if(req.files && Array.isArray(req.files.coverImage)&& req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path;
+    }
+
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required.")
     }
@@ -60,7 +65,7 @@ const registerUser = asyncHandler(async (req,res)=>{
         username:username.toLowerCase()
     })
 
-    const createdUser=await User.findById(User._id).select(
+    const createdUser=await User.findById(user._id).select(
         "-password -refreshToken"//these field will not be given by response
     )
 
